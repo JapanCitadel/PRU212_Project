51653fabdb6f72c9c293da38bfc9cb8cbf7f9d2a
Thiều Thành <110987144+JapanCitadel@users.noreply.github.com>
2024-03-02 11:33:36 +0700

    Edit character movement scripts

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
+    Animator animator;
+    SpriteRenderer sprite;
+    Rigidbody2D rigit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
+        rigit = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsEat", false);
+        animator.SetBool("IsTurn", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPosition.z = 0f;
+
+        transform.position = worldPosition;
+
     }
 }
