8fb775a9e996b6023d8e0ef79271815ce28da7d9
ducbenz <ducbenz2tq@gmail.com>
2024-03-03 23:05:51 +0700

    enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabs;
+    float time;
+    float xScreenWidth;
+    float yScreenHeight;
+    List<GameObject> enemies;
+    EnemyMovement script;
+
+    void Start()
+    {
+        xScreenWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        yScreenHeight = Camera.main.orthographicSize;
+        enemies = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= 3)
+        {
+            time -= 3;
+
+            GameObject enemy = Instantiate(prefabs);
+            enemy.SetActive(true);
+            enemies.Add(enemy);
+
+            float yPosition = Random.Range(-yScreenHeight, yScreenHeight); 
+            enemy.transform.position = new Vector2(
+                Random.Range(-xScreenWidth, xScreenWidth), yPosition);
+
+            script = enemy.GetComponent<EnemyMovement>();
+            if (script != null)
+            {
+                float xSpeed = Random.Range(1, 10);
+                float ySpeed = Random.Range(1, 10);
+
+                if (Random.Range(1, 100) < 50)
+                    script.SetXDirection(1);
+                else
+                    script.SetXDirection(-1);
+
+                if (Random.Range(1, 100) < 50)
+                    script.SetYDirection(1);
+                else
+                    script.SetYDirection(-1);
+
+                script.SetSpeed(xSpeed, ySpeed);
+            }
+        }
+    }
+}
