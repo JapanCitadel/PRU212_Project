7e5dc71341d61f3aa71ad4843853b6786eb218c6
TranBang05 <136000249+TranBang05@users.noreply.github.com>
2024-03-21 17:22:18 +0700

    add music and desgin (#27)
    Co-authored-by: Tranbang <tranxuanbang.yt@gmail.com>
    Co-authored-by: JapanCitadel <110987144+JapanCitadel@users.noreply.github.com>

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource musicAudioSource;
+    public AudioSource vfxAudioSource;
+
+    // tao bien luu tru audio clip
+
+    public AudioClip musicClip;
+    public AudioClip ancaClip;
+
+    void Start()
+    {
+        musicAudioSource.clip = musicClip;
+        musicAudioSource.Play();
+
+    }
+    public void PlaySFX(AudioClip sfxClip)
+    {
+        vfxAudioSource.clip = sfxClip;
+        vfxAudioSource.PlayOneShot(sfxClip);
+    }
+}
